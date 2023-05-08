3f333d9a85bca29181ad22c7fc338e5e6a413e14
David <davidcha@email.unc.edu>
2023-05-08 12:36:23 -0400

    finished adding api endpoints

--- server.js
+++ server.js
@@ -105,4 +105,56 @@ process.on('SIGINT', () => {
 
 import { rps, rpsls } from "./lib/rpsls.js";
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
+// endpoints
+app.get('/app/', (req, res) => {
+  res.status(200);
+  res.send("200 OK");
+});
+
+app.get('/app/rps/', (req, res) => {
+  res.status(200);
+  res.send(rps());
+});
+
+app.get('/app/rpsls/', (req, res) => {
+  res.status(200);
+  res.send(rpsls());
+});
+
+app.get('/app/rps/play/', (req, res) => {
+  res.status(200);
+  res.send(rps(req.query.shot));
+});
+
+app.post('/app/rps/play/', (req, res) => {
+  res.status(200);
+  res.send(rps(req.body.shot));
+});
+
+app.get('/app/rps/play/:shot/', (req, res) => {
+  res.status(200);
+  res.send(rpsls(req.query.shot));
+});
+
+app.get('/app/rpsls/play/', (req, res) => {
+  res.status(200);
+  res.send(rpsls(req.query.shot));
+});
+
+app.post('/app/rpsls/play/', (req, res) => {
+  res.status(200);
+  res.send(rpsls(req.body.shot));
+});
+
+app.get('/app/rpsls/play/:shot/', (req, res) => {
+  res.status(200);
+  res.send(rpsls(req.params.shot));
+});
+
+app.get("*", (req, res) => {
+  res.status(404);
+  res.send("404 NOT FOUND");
+});
